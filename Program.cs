853a938913740e5863b0f2ae5397a660fefa9e0f
room22_07122020 <room22_07122020@outlook.com>
2024-04-13 08:42:45 +0300

    добавил вычесления синуса косинуса тангетца котангетца и вычесления корня

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Security.Policy;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace calculator
 {
@@ -13,59 +9,93 @@ namespace calculator
         {
             double a;
             double b;
-            double result;
+            double c;
             char oper;
             Console.WriteLine("Добро пожаловать в калькулятор!");
             Console.WriteLine("Введите число 'a':");
             a = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine("Введите действие '+','-','*','/':");
+            Console.WriteLine("Введите действие '+', '-', '*', '/', 's' (синус), 'c' (косинус), 't' (тангенс), 'k' (котангенс) , 'r'(корень):");
             oper = Convert.ToChar(Console.ReadLine());
 
-            Console.WriteLine("Введите число 'b':");
-            b = Convert.ToDouble(Console.ReadLine());
+            if (oper == 's')
+
+            {
+                c = Math.Sin(a);
+                Console.WriteLine("Синус числа " + a + " равен " + c);
+            }
+            else if (oper == 'c')
+            {
+                c = Math.Cos(a);
+                Console.WriteLine("Косинус числа " + a + " равен " + c);
+            }
+            else if (oper == 't')
+            {
+                c = Math.Tan(a);
+                Console.WriteLine("Тангенс числа " + a + " равен " + c);
+            }
+            else if (oper == 'k')
+            {
+                c = 1 / Math.Tan(a);
+                Console.WriteLine("Котангенс числа " + a + " равен " + c);
+            }
+            else if (oper == 'r')
+            {
+                if (a < 0)
+                {
+                    Console.WriteLine("Нельзя найти квадратный корень из отрицательного числа.");
+                }
+                else
+                {
+                    c = Math.Sqrt(a);
+                    Console.WriteLine("Корень числа " + a + " равен " + c);
+                }
+            }
+
+            else
+                Console.Write("Введите число b: ");
+            b = Convert.ToSingle(Console.ReadLine());
             if (oper == '+')
             {
-                result = a + b;
-                Console.WriteLine("Cумма " + a + " и " + b + " равна " + result + ".");
-                Console.ReadKey();
+                c = a + b;
+                Console.WriteLine("Сумма ваших чисел равна " + c);
+               
             }
-
             else if (oper == '-')
             {
-                result = a - b;
-                Console.WriteLine("Разность " + a + " и " + b + " равна " + result + ".");
-                Console.ReadKey();
+                c = a - b;
+                Console.WriteLine("Разность ваших чисел равна " + c);
+                
             }
-
             else if (oper == '*')
             {
-                result = a * b;
-                Console.WriteLine("Умножение " + a + " на " + b + " равно " + result + ".");
-                Console.ReadKey();
+                c = a * b;
+                Console.WriteLine("Произведение ваших чисел равно " + c);
+                
             }
-
             else if (oper == '/')
-                {
+            {
                 if (b == 0)
                 {
-                        Console.WriteLine("Деление на 0 невозможноЁ!Ё!Ц!");
-                        Console.ReadKey();
-                    }
-                    else
-                    {
-                        result = a / b;
-                        Console.WriteLine("Деление " + a + " на " + b + " равно " + result + ".");
-                        Console.ReadKey();
-                    }
+                    Console.WriteLine("Ошибка. Делитель не может быть равным нулю.");
+                    
+                }
+                else
+                {
+                    c = a / b;
+                    Console.WriteLine("Частное ваших чисел равно " + c);
+                   
+                }
             }
             else
             {
-                Console.WriteLine("Неизвестный оператор.");
-                Console.ReadKey();
+                Console.WriteLine("Ошибка. Вы ввели неверный знак.");
+                
+
+
             }
 
+            Console.ReadKey();
         }
-
     }
 }
